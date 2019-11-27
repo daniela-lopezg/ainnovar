b1006db5b5bd25f6503a74384c8d85e8c3972f9b
Daniela López <daniela.lopezg@gmail.com>
2019-11-27 20:14:19 -0300

    Added how to identify if the child is a bully or bullied.

---
+++ Child_data.py
@@ -0,0 +1,46 @@
+class Child_data:
+	def __init__(self,id,name, parent, child_province, child_age):
+		self.userid = id
+		self.fullname = name
+		self.parentname = parent
+		self.province = child_province
+		self.age = child_age
+		self.bullied_score = 0.0
+		self.bully_score = 0.0
+		self.interactions_total = 0.0
+		self.interactions_bullied = 0.0
+		self.interactions_bully = 0.0
+
+	def set_bully_score(self):
+		if(self.interactions_total == 0.0):
+			return 0.0
+		self.bully_score = self.interactions_bully/self.interactions_total
+
+	def set_bullied_score(self):
+		if(self.interactions_total == 0.0):
+			return 0.0
+		self.bullied_score = self.interactions_bullied/self.interactions_total
+
+	def increase_interactions_bully(self):
+		self.interactions_bully = self.interactions_bully + 1.0
+
+	def increase_interactions_bullied(self):
+		self.interactions_bullied = self.interactions_bullied + 1.0
+
+	def increase_interactions_total(self):
+		self.interactions_total = self.interactions_total + 1.0
+
+	def getFullname(self):
+		return self.fullname
+
+	def getUserid(self):
+		return self.userid
+
+	def getBullied_Score(self):
+		return self.bullied_score
+
+	def getBully_Score(self):
+		return self.bully_score
+
+	def getInteractions_total(self):
+		return self.interactions_total
--- model.py
+++ model.py
@@ -3,6 +3,8 @@
 import csv
 import nltk
 import re
+import sys
+from Child_data import *
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.data import load
@@ -17,6 +19,8 @@ non_words = list(punctuation)
 non_words.extend(['¿', '¡'])
 non_words.extend(map(str,range(10)))
 
+# score that a phrase has to have to be negative
+threshold = float(sys.argv[1])
 
 def tokenize(text):
 	# remove links from tweets
@@ -33,6 +37,33 @@ def tokenize(text):
 			final_words.append(word)
 	return final_words                                    
 
+def get_children(child_list, userid):
+	index = 0
+	for child in child_list:
+		child_userid = child.getUserid()
+		if(userid == child_userid):
+			return index
+		index = index + 1
+	return -1
+
+def increase_interactions(child_list, userid):
+	index = get_children(child_list, userid)
+	child_list[int(userid)].increase_interactions_total()
+	print(child_list[int(userid)].getInteractions_total())	
+
+def increase_bully_score(child_list,userid):
+	index = get_children(child_list, userid)
+	child_list[int(userid)].increase_interactions_bully()
+	message = str(child_list[int(userid)].getFullname()) + " hace bullying"
+	print(message)
+
+def increase_bullied_score(child_list, userid):
+	index = get_children(child_list, userid)
+	child_list[int(userid)].increase_interactions_bullied()
+	message = str(child_list[int(userid)].getFullname()) + " recibe bullying"
+	print(message)
+
+
 negative_words = []
 negative_words_value = []
 with open('dataset_bulling_demo.xlsx - palabras.csv', 'r') as csvFile:
@@ -42,24 +73,64 @@ with open('dataset_bulling_demo.xlsx - palabras.csv', 'r') as csvFile:
 		negative_words_value.append(row[1])
 csvFile.close()
 
-#print(negative_words)
-#print(negative_words_value)
+children_list = []
 
-#print("#####################################")
+with open('dataset_bulling_demo.xlsx - personajes.csv', 'r') as csvFile:
+	reader = csv.reader(csvFile)
+	for row in reader:
+		child = Child_data(row[0], row[1], row[2], row[3], row[4])
+		children_list.append(child)
+csvFile.close()
+
 
 with open('dataset_bulling_demo.xlsx - dataset.csv', 'r') as csvFile:
 	    reader = csv.reader(csvFile)
+	    count = 0
 	    for row in reader:
+		if(count == 0):
+			count = -1
+			continue
+		negative_phrase = False
 	        phrase=row[4]
+		emisor = int(row[5])
 		tokenized_phrase = tokenize(phrase)
 		phrase_score = 0.0;
-		index = 0;
+#		index = 0;
 		for word in tokenized_phrase:
 			if word in negative_words:
 				phrase_score = phrase_score + 1; #float(negative_words_value[index]);
-			index += 1
+#			index += 1
+		message = "En la frase \"" + phrase + "\""		
+		print(message)
+		increase_interactions(children_list, row[0])
+		if(phrase_score >= threshold):
+			negative_phrase = True
+		if(negative_phrase and emisor == 1):
+			increase_bully_score(children_list, row[0])
+		elif(negative_phrase and emisor == 0):
+			increase_bullied_score(children_list, row[0])
+		else:
+			print("Nadie recibió bullying")
 		message = "Frase \"" + phrase +  "\" es " + str(100*phrase_score/len(tokenized_phrase)) + "% negativa"
 		print(message)
+		print("#############################################################################")
 
 csvFile.close()
 
+for child in children_list:
+	child.set_bully_score()
+	child.set_bullied_score()
+	child_bullied_score = child.getBullied_Score()
+	child_bully_score = child.getBully_Score()
+	if(child_bullied_score != 0.0):
+		message = child.getFullname() + " recibe bullying con un puntaje de " + str(child_bullied_score*100) + "%"
+		print(message)
+	else:
+		message = child.getFullname() + " no recibe bullying"
+		print(message)
+	if(child_bully_score != 0.0):
+		message = child.getFullname() + " es un bully con un puntaje de " + str(child_bully_score*100) + "%"
+		print(message)
+	else:
+		message = child.getFullname() + " no es un bully"
+		print(message)
